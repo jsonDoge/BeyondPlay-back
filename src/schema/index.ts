8b608cf2feb2b737249b9a8d53a9caedb74bdb76
jsonDoge <matas.jasonas@protonmail.com>
2024-01-15 01:37:24 +0200

    feat: initial stitching failing with unclear reason - always null

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,4 +1,12 @@
+import { stitchSchemas } from '@graphql-tools/stitch';
+
+import { liftSubschema } from './lift';
+import { countrySubschema } from './country';
 import { logsGQLschema } from './log';
 
+const gatewaySchema = stitchSchemas({
+  subschemas: [liftSubschema, countrySubschema, { schema: logsGQLschema }],
+});
+
 // TODO: more schema to be merged
-export const mergedGQLSchema = logsGQLschema;
+export const mergedGQLSchema = gatewaySchema;
